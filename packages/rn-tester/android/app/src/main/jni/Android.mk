4c59387b1a117f7dac80fc0285a3eb708d889511
jarzola <jarzola@evernote.com>
2021-09-23 14:46:21 -0700

    Move all modules under the modules folder

--- packages/rn-tester/android/app/src/main/jni/Android.mk
+++ packages/rn-tester/android/app/src/main/jni/Android.mk
@@ -10,12 +10,13 @@ include $(REACT_ANDROID_DIR)/Android-prebuilt.mk
 # SampleNativeModule
 include $(REACT_COMMON_DIR)/react/nativemodule/samples/platform/android/Android.mk
 
-# start evernote
+# start EvernoteTurboModule
 CONTEXT := evernote
 #$(warning $(CONTEXT) $(THIS_DIR)) 
-EVERNOTE_MODULE_PATH := $(THIS_DIR)/generated
-EVERNOTE_MODULE_JNI_PATH := $(EVERNOTE_MODULE_PATH)/jni
-include $(EVERNOTE_MODULE_PATH)/jni/Android.mk
+MODULE_NAME := EvernoteTurboModule
+MODULE_PATH := $(THIS_DIR)/modules/$(MODULE_NAME)/generated
+MODULE_JNI_PATH := $(MODULE_PATH)/jni
+include $(MODULE_PATH)/jni/Android.mk
 # end Evernote
 
 LOCAL_PATH := $(THIS_DIR)
@@ -23,7 +24,7 @@ LOCAL_PATH := $(THIS_DIR)
 include $(CLEAR_VARS)
 LOCAL_MODULE := rntester_appmodules
 # Note: Build the react-native-codegen output along with other app-specific C++ files.
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(GENERATED_SRC_DIR)/codegen/jni $(EVERNOTE_MODULE_JNI_PATH)
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(GENERATED_SRC_DIR)/codegen/jni $(MODULE_JNI_PATH)
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp) $(wildcard $(GENERATED_SRC_DIR)/codegen/jni/*.cpp)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) $(GENERATED_SRC_DIR)/codegen/jni
 LOCAL_SHARED_LIBRARIES := libfbjni libglog libfolly_json libyoga libreact_nativemodule_core libturbomodulejsijni librrc_view libreact_render_core libreact_render_graphics libreact_codegen_rncore 
